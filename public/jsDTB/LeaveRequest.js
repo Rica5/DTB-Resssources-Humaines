7c03c74e41a57e6d889dabf68f09456ec8316b5a
Onisoa Tahiana RAZANAKINIAINA <yunis.thn@gmail.com>
2024-01-25 13:01:34 +0300

    update demande de congé

--- public/jsDTB/LeaveRequest.js
+++ public/jsDTB/LeaveRequest.js
@@ -29,3 +29,29 @@ $("#myUpcoming").click(() =>{
     $("#container-upcoming").show()
     $("#container-make").hide()
 })
+
+
+
+
+$("#envoye-demande").on('click', function() {
+    var nom = $("#nom").text();
+    var matricule = $("#matricule").text();
+    var mcode = $("#mcode").text();
+    var dateDebut = $("#date-debut").val();
+    var heureDebut = $("#heure-debut").val();
+    var dateFin = $("#date-fin").val();
+    var heureFin = $("#heure-fin").val();
+    var motif = $("#motif").val().trim();
+    var recuperation = $("#recuperation").val().trim();
+
+    // Vous pouvez utiliser ces valeurs comme nécessaire
+    console.log("Nom:", nom);
+    console.log("Matricule:", matricule);
+    console.log("M-code:", mcode);
+    console.log("Date début:", dateDebut);
+    console.log("Heure début:", heureDebut);
+    console.log("Date fin:", dateFin);
+    console.log("Heure fin:", heureFin);
+    console.log("Motif:", motif);
+    console.log("Récupération:", recuperation);
+});
