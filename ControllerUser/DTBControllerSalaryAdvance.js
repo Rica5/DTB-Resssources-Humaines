f06ffa27325ba40669bb6993daaa06a98cbcef96
Onisoa Tahiana RAZANAKINIAINA <onisoa.pdfb-001@solumada.local>
2024-09-02 14:53:05 +0300

    update salary

--- ControllerUser/DTBControllerSalaryAdvance.js
+++ ControllerUser/DTBControllerSalaryAdvance.js
@@ -12,12 +12,21 @@ const crypto = require("crypto");
 const moment = require("moment");
 const fs = require("fs");
 
+const months = [ "janvier", "février", "mars", "avril", "mai", "juin",
+    "juillet", "août", "septembre", "octobre", "novembre", "décembre"];
+const currentYear = new Date().getFullYear() + 1;
+const years = [];
+for (let year = 2021; year <= currentYear; year++) {
+    years.push(year);
+}
 //Home page
 const getSalaryAdvance = async (req,res) => {
     var session = req.session;
    if ( session.occupation_u == "User"){
         var user = await UserSchema.findOne({ m_code: session.m_code });   
-        res.render("PageEmployee/AvanceSalaire.html",{codeUser:session.m_code, user});
+        // console.log("user", user);
+        
+        res.render("PageEmployee/AvanceSalaire.html",{codeUser:session.m_code, user, months, years});
    }
    else {
     res.send("Bad authentification please log in");
