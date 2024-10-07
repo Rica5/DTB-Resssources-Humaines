9820437cf6e951915e440ce8a01651850fb38f79
Njato <m.pdfb002.njato.optimumsolutions@gmail.com>
2024-10-07 09:24:01 +0300

    Exporter fichier

--- app.js
+++ app.js
@@ -68,7 +68,7 @@ async function copyNonExistingRecords(sourceCollectionName, destinationCollectio
     // Fetch all records from the source collection
     const sourceRecords = await SourceCollection.find({
       date_start: { 
-        $regex: "^2023-01" // (2024 volana janvier ) 2023 fotsiny raha année iray 
+        $regex: "^2024" // (2024 volana janvier ) 2023 fotsiny raha année iray 
       }
     }).toArray();
 
